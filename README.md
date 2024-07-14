64bacab346c477cb4706f98204a627d9451ce148
umi <yana3umi@gmail.com>
2024-07-15 02:08:49 +0900

    1st

---
+++ README.md
@@ -0,0 +1,21 @@
+# 個人設定用ディレクトリ
+
+<!-- VSCODE -->
+## vscode
+
+settings.json
+```
+
+```
+
+[tarmina](https://qiita.com/takahito0508/items/cf820e49b63aea11cd52)l
+
+```
+
+```
+## pre-commit
+```
+$ pre-commit sample-config > .pre-commit-config.yaml
+
+
+```
---
+++ brewlist/brew-list.sh
@@ -0,0 +1,17 @@
+=> Formulae
+act                     gettext                 jansson                 libtasn1                npth                    pre-commit              tree-sitter
+autoconf                gh                      libassuan               libunistring            openldap                pyenv                   unbound
+awscli                  git                     libevent                libusb                  openssl@3               readline                xz
+ca-certificates         git-secrets             libgcrypt               libyaml                 p11-kit                 sqlite                  zsh-autosuggestions
+cask                    gmp                     libgpg-error            m4                      pcre2                   tfenv                   zsh-completions
+coreutils               gnupg                   libidn2                 mas                     pinentry                tflint                  zsh-syntax-highlighting
+direnv                  gnutls                  libksba                 mpdecimal               pkg-config              tfsec
+emacs                   grep                    libnghttp2              nettle                  powerlevel10k           tree
+
+==> Casks
+1password                       docker                          keyboardcleantool               runcat-plugins-manager          visual-studio-code
+appcleaner                      focus                           kindle                          safeincloud-password-manager    yt-music
+bettertouchtool                 font-jetbrains-mono             logi-options-plus               sequel-ace                      zoom
+clipy                           google-chrome                   mobster                         slack
+cmd-eikana                      google-cloud-sdk                notion                          spotify
+displaylink                     iterm2                          popclip                         steam
